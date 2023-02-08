3cb566c4288185fca6f917c677f7ba68f189495a
Hannah Rudt <hgr26@cornell.edu>
2023-02-08 16:06:22 -0500

    added flder

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from whitenoise import WhiteNoise
 
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, 
-                          root='static/', 
-                          prefix='static/', 
+                          root='static/', # use the folder name 
+                          prefix='/', 
                           index_file='index.htm', 
                           autorefresh=True)
 
-@app.route('/', methods=['GET'])
-def hello():
-    # return make_response("Hello, world!!!!!!!!!!!!!!!!")
-    render_template("..\static\index.html")
+# @app.route('/', methods=['GET'])
+# def hello():
+#     return make_response("Hello, world!!!!!!!!!!!!!!!!")
+#     # render_template("..\static\index.html")
     
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
