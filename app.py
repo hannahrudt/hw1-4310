da0f0594ac56a60169d0d9436c1eca65dac48259
Hannah Rudt <hgr26@cornell.edu>
2023-02-08 19:39:55 -0500

    modified app.py

--- app.py
+++ app.py
@@ -3,15 +3,10 @@ from whitenoise import WhiteNoise
 
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, 
-                          root='static/', # use the folder name 
+                          root='hw1/', # use the folder name 
                           prefix='/', 
-                          index_file='index.htm', 
+                          index_file='index.html', 
                           autorefresh=True)
-
-# @app.route('/', methods=['GET'])
-# def hello():
-#     return make_response("Hello, world!!!!!!!!!!!!!!!!")
-#     # render_template("..\static\index.html")
     
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
